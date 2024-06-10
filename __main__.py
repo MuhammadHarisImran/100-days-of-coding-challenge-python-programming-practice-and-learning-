b63e54320e7935f5f051527dbd7b43afdaf0f08e
MuhammadHarisImran <mharisimran24@gmail.com>
2024-06-10 12:27:56 +0500

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,36 @@
+print("hello world")
+print("this is the beginning of the 100 days of code challenge")
+print("so we print ('print(this is  written inside the parenthese')")
+
+
+# so now lets do some string manipulation following the course as we go
+print("haris"+"imran")
+#so as java the + is for concatination in our string
+#another thing is that if the code is not indented well it will not run
+#  print("hello world!")   #this will not run because of the indentation error
+
+print("this is how u print an operator "+" in the output string")
+
+#so now we see how the input works
+#input("Hello what is yout name?\n")
+
+#print("hello"+ input("what is your name?"))
+
+#print(len(input("enter your name")))
+#so this code will take input calculate its lenght and then print this the lenght using the print function but it will also count the spaces in your input
+#so if your input is m haris imran then the output will be 13 because of the added amount of spaces
+
+
+name = input("enter your name") #so these are python variables
+print(name)
+
+a = input("enter 1st number")
+b = input("enter 2nd number")
+print("so now lets switch")
+
+c = b
+b = a
+a = c
+
+print("a=" + a +"\n b=" + b)
+
