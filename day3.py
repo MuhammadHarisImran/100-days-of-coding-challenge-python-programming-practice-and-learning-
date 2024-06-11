d8b67965e94f3eeaac4f16c65503395a1ebec44e
MuhammadHarisImran <mharisimran24@gmail.com>
2024-06-11 13:30:54 +0500

    Add files via upload
    day 3 practice and code

---
+++ day3.py
@@ -0,0 +1,27 @@
+#so its day 3 and we are learing about control flow
+
+number = int(input("please enter a number = "))
+
+if number / 2 == 0:
+    print("the entered number is even")
+else:
+    print("the number is odd")
+
+#this task was to get familiar with if else statements in python
+
+#<================ Task 2 =================>
+height = float(input("please enter your height in meters :"))
+weight = float(input("please enter your weight in kilograms :"))
+
+BMI = weight / height ** 2
+
+if BMI < 18.5:
+    print("your are underweight")
+elif BMI <=25:
+    print("your weight is normal")
+elif BMI <=30:
+    print("you are overweight")
+elif BMI <=35:
+    print("you are obese")
+elif BMI > 35:
+    print("you are in danger")
