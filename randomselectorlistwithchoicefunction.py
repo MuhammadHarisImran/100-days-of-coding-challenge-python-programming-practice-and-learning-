b0397998e585a8425206a18ef36120a56b228b27
MuhammadHarisImran <mharisimran24@gmail.com>
2024-06-12 13:39:10 +0500

    Delete randomselectorlistwithchoicefunction.py

--- randomselectorlistwithchoicefunction.py
+++
@@ -1,9 +0,0 @@
-import random
-
-names_str = input("enter names seperated by commas:")
-names = names_str.split(",")
-num_items= len(names)
-person_paying = random.choice(names)
-#random_select = random.randint(0,len(names))
-#person_paying = names[random_select]
-print(f"{person_paying} is paying the bill")
